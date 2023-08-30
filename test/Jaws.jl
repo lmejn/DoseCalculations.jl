0e8f137029a5b64dfd772cc87f171ded9e8d8252
Lars Mejnertsen <lars.mejnertsen@sydney.edu.au>
2023-08-30 12:55:01 +1000

    Tidied Jaws tests

--- test/Jaws.jl
+++ test/Jaws.jl
@@ -1,22 +1,35 @@
 @testset "Jaws" begin
 
-    x = rand(2)
-    y = rand(2)
-    jaws = Jaws(x, y)
-    
-    @test getx(jaws) == x
-    @test gety(jaws) == y
-    
-    @test jaws == Jaws(x[1], x[2], y[1], y[2])
-    
-    fieldsize = rand()
-    jaws = Jaws(fieldsize)
-    @test getx(jaws) == 0.5*fieldsize*[-1, 1]
-    @test gety(jaws) == 0.5*fieldsize*[-1, 1]
+    @testset "Constructors" begin
 
+        x, y = rand(2), rand(2)
+        jaws = Jaws(x, y)
+
+        @test jaws.x == x
+        @test jaws.y == y
+        
+        @test jaws == Jaws(x[1], x[2], y[1], y[2])
+        
+        fieldsize = rand()
+        jaws = Jaws(fieldsize)
+        @test getx(jaws) == 0.5*fieldsize*[-1, 1]
+        @test gety(jaws) == 0.5*fieldsize*[-1, 1]
+    end
+
+    @testset "Methods" begin
+        x, y = rand(2), rand(2)
+        jaws = Jaws(x, y) 
+
+        @test getx(jaws) == x
+        @test gety(jaws) == y
+        @test getpositions(jaws) == (x, y)
+
+        @test getarea(jaws) == (x[2]-x[1])*(y[2]-y[1])
+    end
 
     @testset "Intersection (∩)" begin
-        jaws = Jaws(20.)
+        fieldsize = 20.
+        jaws = Jaws(fieldsize)
 
         x2, y2 =[-9., 12.], [-6., 13.]
         jaws2 = Jaws(x2, y2) ∩ jaws
